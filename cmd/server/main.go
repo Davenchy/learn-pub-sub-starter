86f9be40b4157dd81d7bb986f6adc10de70de6f3
Fadi Asaad <firon1222@gmail.com>
2025-07-26 11:12:35 +0300

    create AMQP connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	RMQ_URL := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(RMQ_URL)
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ:", err)
+	}
+	defer conn.Close()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down Peril server...")
 }
