ce42873e0d720268187bf91223268c34eb7e6c5b
Fadi Asaad <firon1222@gmail.com>
2025-07-26 11:45:28 +0300

    publish json payloads to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,20 @@ func main() {
 		log.Fatal("Failed to connect to RabbitMQ:", err)
 	}
 	defer conn.Close()
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Failed to open a channel:", err)
+	}
+	defer channel.Close()
+
+	if err := pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true}); err != nil {
+		log.Fatal("Failed to publish message:", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
